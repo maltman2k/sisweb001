6005273addf7b6f9a7469f4f9eac9de2696b7bab
maltman2k <maltman2k@hotmail.com>
2017-03-16 11:42:49 +0100

    first

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+var port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/public/index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile(__dirname + '/public/sitemap.html');
+});
+
+app.listen(port);
+
+console.log("Running at Port "+port);
+
+
+/*
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello, world!</h1>');
+    
+}).listen(process.env.PORT || 8080);
+*/
